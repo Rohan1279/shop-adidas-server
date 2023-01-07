047382d45299b61d4e4c8b86b416e02cab724b6a
Rohan1279 <muniruddinrohan@gmail.com>
2023-01-07 22:09:10 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.port || 5000;
+require("dotenv").config();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("simple nodeserver running");
+});
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lgtzpwm.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+// add below codes
+async function run() {
+  try {
+    const productsCollection = client
+      .db("shop-adidas-db")
+      .collection("all-products");
+    //read
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const cursor = productsCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+  } finally {
+    //don't add close()
+  }
+}
+run().catch((err) => console.log(err));
+
+app.listen(port, () => {
+  // after adding mongo db uri
+  client.connect((err) => {
+    if (err) {
+      //catch database error
+      console.log(err);
+    } else {
+      console.log("Connected to MongoDB");
+    }
+  });
+  console.log(`simple node server running on port ${port}`);
+});
