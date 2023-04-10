797cee1e2629eb8daf4cd3147f0f86fd255e7cd4
Rohan1279 <muniruddinrohan@gmail.com>
2023-04-11 04:19:45 +0600

    /tmp added

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const drive = google.drive({
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./");
+    cb(null, "/tmp");
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
