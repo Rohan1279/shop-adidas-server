7fb1a82d006e15432c8a730237a9e2caeffb71f6
Rohan1279 <muniruddinrohan@gmail.com>
2023-06-14 10:36:47 +0600

    add sorting for seller products

--- index.js
+++ index.js
@@ -199,60 +199,66 @@ async function run() {
     // });
 
     app.get("/seller_products", async (req, res) => {
-      // const sortOrder = {};
-      // const {
-      //   email,
-      //   currentPage,
-      //   limit,
-      //   dateOrder,
-      //   priceOrder,
-      //   search = "",
-      // } = req.query;
-      // console.log("---------------------------");
+      const sortOrder = {};
+      const {
+        email,
+        currentPage,
+        limit,
+        dateOrder,
+        priceOrder,
+        ratingsOrder,
+        search = "",
+      } = req.query;
+      console.log("---------------------------");
 
-      // // console.log("dateOrder", parseInt(dateOrder));
-      // // console.log("priceOrder", parseInt(priceOrder));
-      // if (!parseInt(priceOrder)) {
-      //   sortOrder.posted_on = parseInt(dateOrder);
-      // } else if (!parseInt(dateOrder)) {
-      //   sortOrder.price = parseInt(priceOrder);
-      // }
-      // // console.log(search);
+      console.log("dateOrder", parseInt(dateOrder));
+      console.log("priceOrder", parseInt(priceOrder));
+      console.log("ratingsOrder", parseInt(ratingsOrder));
 
-      // const query = {
-      //   seller_email: email,
-      //   $or: [
-      //     { name: { $regex: search, $options: "i" } }, // search by product name
-      //     { category: { $regex: search, $options: "i" } }, // search by category
-      //     { description: { $regex: search, $options: "i" } }, // search by description
-      //     { brand: { $regex: search, $options: "i" } }, // search by brand
-      //     // { posted_on: { $regex: search, $options: "i" } }, // search by date
-      //     { color: { $regex: search, $options: "i" } }, // search by color
-      //   ],
-      // };
-      // const products = await productsCollection
-      //   .find(query)
-      //   // -1 -> descending ||  1 -> ascending
-      //   .sort(sortOrder)
-      //   .skip(parseInt(currentPage) * parseInt(limit))
-      //   .limit(parseInt(limit))
-      //   .toArray();
-      // const count = await productsCollection.find(query).toArray();
+      if (!parseInt(priceOrder) && !parseInt(ratingsOrder)) {
+        sortOrder.posted_on = parseInt(dateOrder);
+      } else if (!parseInt(dateOrder) && !parseInt(ratingsOrder)) {
+        sortOrder.price = parseInt(priceOrder);
+      } else if (!parseInt(priceOrder) && !parseInt(dateOrder)) {
+        sortOrder.ratings = parseInt(ratingsOrder);
+      }
 
-      // products.map((product) => {
-      //   // console.log(product?.name);
-      //   // console.log(product?.category);
-      // });
-      // console.log("---------------------------");
+      // console.log(sortOrder);
+      const query = {
+        seller_email: email,
+        $or: [
+          { name: { $regex: search, $options: "i" } }, // search by product name
+          { category: { $regex: search, $options: "i" } }, // search by category
+          { description: { $regex: search, $options: "i" } }, // search by description
+          { brand: { $regex: search, $options: "i" } }, // search by brand
+          // { posted_on: { $regex: search, $options: "i" } }, // search by date
+          { color: { $regex: search, $options: "i" } }, // search by color
+        ],
+      };
+      const products = await productsCollection
+        .find(query)
+        // -1 -> descending ||  1 -> ascending
+        .sort(sortOrder)
+        .skip(parseInt(currentPage) * parseInt(limit))
+        .limit(parseInt(limit))
+        .toArray();
+      const count = await productsCollection.find(query).toArray();
 
-      // res.send([...products, { count: count?.length }]);
+      // console.log(products?.length);
+      products.map((product) => {
+        // console.log(product?.name);
+        // console.log(product?.category);
+      });
+      console.log("---------------------------");
 
       // Update the document
-      const result = await productsCollection.update(
-        { seller: "Adidas" },
-        { $set: { seller_email: "adidas@adidas.com" } }
-      );
-      console.log(result);
+      // const result = await productsCollection.updateMany(
+      //   { seller: "Adidas" },
+      //   { $set: { brand: "Adidas" } }
+      // );
+      //   console.log(result);
+
+      res.send([...products, { count: count?.length }]);
     });
     // ! POST
     app.put("/user/:email", async (req, res) => {
@@ -429,7 +435,6 @@ async function run() {
         }
       });
     });
-
     // temporary to add property
     // app.get("/addData/stock", async (req, res) => {
     //   const filter = {};
